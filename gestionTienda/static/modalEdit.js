1bcacf361900a7094c0c4a6063a2880c360c0dbb
Luis Antonio Orbegoso Moreno <antoniorbegosomoreno@gmail.com>
2023-08-06 17:03:37 -0500

    Creación de las páginas y vistas del proyecto

---
+++ gestionTienda/static/modalEdit.js
@@ -0,0 +1,29 @@
+;(function () {
+    const modal = new bootstrap.Modal(document.getElementById("modal"))
+    const modaldelete = new bootstrap.Modal(document.getElementById("modalDelete"))
+  
+    htmx.on("htmx:afterSwap", (e) => {
+      if (e.detail.target.id == "editar") {
+        modal.show()
+      }
+      if (e.detail.target.id == "borrar") {
+        modaldelete.show()
+      }
+    })
+  
+    htmx.on("htmx:beforeSwap", (e) => {
+      if (e.detail.target.id == "editar" && !e.detail.xhr.response) {
+        modal.hide()
+        e.detail.shouldSwap = false
+      }
+      if (e.detail.target.id == "borrar" && !e.detail.xhr.response) {
+        modaldelete.hide()
+        e.detail.shouldSwap = false
+      }
+    })
+  
+    htmx.on("hidden.bs.modal", () => {
+      document.getElementById("editar").innerHTML = ""
+      document.getElementById("borrar").innerHTML = ""
+    })
+  })()
